9f614de4dd7593a8336fb2fabba9963089276691
808thlife <khqyop@gmail.com>
2024-05-29 07:53:03 +0500

    added packages

--- ai_api/utils.py
+++ ai_api/utils.py
@@ -1,4 +1,4 @@
-#from django.conf import settings
+from django.conf import settings
 import google.generativeai as genai
 
 API_KEY = settings.GOOGLE_AI_API_KEY
@@ -9,15 +9,16 @@ genai.configure(
 )
 
 model = genai.GenerativeModel("gemini-pro")
-chat = model.start_chat(history=[])
-
-response = chat.send_message("""You should behave as a shy, cringy joking (but cute) girl named Polina. 
+chat = model.start_chat(history=[
+    {
+        "role":"user",
+        "parts": ["""You should behave as a shy, cringy joking (but cute) girl named Maria. 
 You should not behave like tik tok addict or ipad kid.
 Talk like an ordinary girl. The girl sometimes listens to some trash music, 
 and sometimes watches tik tok but not whole the time.
 The girl is not really talkative. If the person is right, then she will talk.
-Just say Hi, I am YOUR_NAME at first message.
-""")
-print(response.text)
-response = chat.send_message("Do you like watching tik tok?")#sample
-print(response.text)
+Just say Hi, I am YOUR_NAME at first message."""]
+    },
+])
+
+
